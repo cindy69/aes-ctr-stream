a468dd3024400dd268d793d9ffaaaae0097609b0
Bo Zhu <github@bozhu.email>
2014-09-18 20:24:11 -0400

    try to use aes-cbc decryption to implement aes-ctr encryption, but it turns out the used pkcs#7 padding scheme may not be easily bypassed

--- aes-ctr.js
+++ aes-ctr.js
@@ -1,85 +1,104 @@
 (function() {
-    function AesEcb(key, plaintext) {
-        return crypto.subtle.encrypt(
-            {name: 'aes-cbc', iv: new Uint8Array(16)},
-            key,
-            plaintext
-        );
-    }
+    'use strict';
 
-    function genBlock(key, counter) {
-        return AesEcb(key, counter).then(
-            function(ciphertext) {
-                // update counter
-                var i;
-                for (i = counter.byteLength - 1; i > -1; i--) {
-                    counter[i] += 1;
-                    if (counter[i] != 0) {
-                        break;
-                    }
+    function genKeyStream(key, counter, length, offset) {
+        var totalLength = length + offset;
+        var numBlocks = Math.ceil(totalLength / 16);
+        var ciphertext = new Uint8Array(numBlocks * 16);
+        var i, j;
+
+        // use counters as ciphertexts
+        ciphertext.set(counter);
+        var idx;
+        for (i = 1; i < numBlocks; i++) {
+            for (j = 0; j < 16; j++) {
+                ciphertext[16 * i + j] = ciphertext[16 * (i - 1) + j];
+            }
+            for (j = 15; j > -1; j--) {
+                idx = 16 * i + j;
+                ciphertext[idx] += 1;
+                if (ciphertext[idx] !== 0) {
+                    break;
                 }
-                return new Uint8Array(ciphertext);
+            }
+        }
+
+        // update counter
+        for (i = 0; i < 16; i++) {
+            counter[i] = ciphertext[(numBlocks - 1) * 16 + i];
+        }
+
+        return crypto.subtle.decrypt(
+            {
+                name: 'aes-cbc',
+                iv: new Uint8Array(16)
             },
-            console.error.bind(console, 'AesEcb error')
+            key,
+            ciphertext
+        ).then(
+            function(plaintext) {
+                plaintext = new Uint8Array(plaintext);
+
+                // XOR counters with plaintexts to get the AES-CTR key stream we want
+                var i;
+                for (i = 0; i < (numBlocks - 1) * 16; i++) {
+                    plaintext[16 + i] ^= ciphertext[i];
+                }
+
+                return new Uint8Array(plaintext.buffer, offset, length);
+            },
+            console.error.bind(console, 'decrypt error')
         );
     }
 
 
     var AesCtr = function() {};
 
-    AesCtr.prototype.init = function(keyData, initCtr) {
-        this.counter = initCtr;
-
+    AesCtr.prototype.init = function(keyData, initCounter) {
         var self = this;
+        self.counter = initCounter;
+        self.offset = 0;
 
         return crypto.subtle.importKey(
             'raw', keyData,
-            {name: 'aes-cbc', iv: new Uint8Array(16)},
-            true, ['encrypt']
+            {
+                name: 'aes-cbc',
+                iv: new Uint8Array(16)
+            },
+            false, ['decrypt']
         ).then(
             function(key) {
                 self.key = key;
-                return genBlock(self.key, self.counter);
             },
             console.error.bind(console, 'Cannot import key')
-        ).then(
-            function(ciphertext) {
-                self.offset = 0;
-                self.stream = ciphertext;
-            },
-            console.error.bind(console, 'genBlock error')
         );
     };
 
     AesCtr.prototype.update = function(bytes) {
         var self = this;
+        var length = bytes.byteLength;
 
-        function updateByte(idx) {
-            bytes[idx] ^= self.stream[self.offset++];
-            
-            if (self.offset === 16) {
-                return genBlock(self.key, self.counter).then(
-                    function(ciphertext) {
-                        self.offset = 0;
-                        self.stream = ciphertext;
-                        return idx + 1;
-                    },
-                    console.error.bind(console, 'genBlock error')
-                )
-            }
+        return genKeyStream(this.key, this.counter, length, this.offset).then(
+            function(keyStream) {
+                var i;
+                for (i = 0; i < length; i++) {
+                    bytes[i] ^= keyStream[i];
+                }
 
-            return idx + 1;
-        }
-
-        var i, sequence = Promise.resolve(0);
-        for (i = 0; i < bytes.byteLength; i++) {
-            sequence = sequence.then(
-                updateByte,
-                console.error.bind(console, 'sequence error')
-            );
-        }
-
-        return sequence;
+                // update offset
+                self.offset = (self.offset + length) % 16;
+                if (self.offset === 0) {
+                    // update counter
+                    for (i = 15; i > -1; i--) {
+                        self.counter[i] += 1;
+                        if (self.counter[i] !== 0) {
+                            break;
+                        }
+                    }
+                }
+            },
+            console.error.bind(console, 'genKeyStream error')
+        );
     };
 
     window.AesCtr = AesCtr;
